d570896e09f3f258bb2d7cf89a0c9c2719d90414
박건우 <white0581@naver.com>
2023-11-24 00:37:07 +0900

    feat: Suspense와 Transition의 조합

--- src/components/ReactQuery.tsx
+++ src/components/ReactQuery.tsx
@@ -1,22 +1,53 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useState, useTransition } from 'react'
 import Sidebar from './Sidebar'
 import AlbumList from './AlbumList'
 import TodoList from './TodoList'
 import { ErrorBoundary } from 'react-error-boundary'
 
+type Tabs = 'album' | 'todo'
+
 export default function ReactQuery() {
+  const [selectedTab, setSelectedTab] = useState<Tabs>('album')
+  const [isPending, startTransition] = useTransition()
+
+  const onClickTab = (tab: Tabs) => {
+    startTransition(() => setSelectedTab(tab))
+  }
+
   return (
     <div className="flex p-4">
       <Sidebar />
       <div className="flex-1">
+        <div className="flex justify-center gap-2">
+          <button
+            className={`p-2 border-2
+            ${
+              selectedTab === 'album'
+                ? 'border-orange-400'
+                : 'border-transparent'
+            }
+            ${isPending ? 'opacity-20' : 'opacity-100'}`}
+            onClick={() => onClickTab('album')}
+          >
+            Album
+          </button>
+          <button
+            className={`p-2 border-2
+            ${
+              selectedTab === 'todo'
+                ? 'border-orange-400'
+                : 'border-transparent'
+            }
+            ${isPending ? 'opacity-20' : 'opacity-100'}`}
+            onClick={() => onClickTab('todo')}
+          >
+            Todo
+          </button>
+        </div>
+
         <ErrorBoundary fallback={<p>Album 리스트 에러입니다!</p>}>
           <Suspense fallback={<p>Album 리스트 로딩중...</p>}>
-            <AlbumList />
-          </Suspense>
-        </ErrorBoundary>
-        <ErrorBoundary fallback={<p>Todo 에러입니다!</p>}>
-          <Suspense fallback={<p>Todo 리스트 로딩중...</p>}>
-            <TodoList />
+            {selectedTab === 'album' ? <AlbumList /> : <TodoList />}
           </Suspense>
         </ErrorBoundary>
       </div>
