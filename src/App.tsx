757cf39fe139f18630abf3c9805a77dc784e91b6
박건우 <white0581@naver.com>
2023-11-23 22:13:00 +0900

    build: Axios, React-Error-Boundary, React-Query 설치 및 세팅,

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import AutoBatchEventHandler from './components/AutoBatchEventHandler'
-// import AutoBatchOther from './components/AutoBatchOther'
+import AutoBatchOther from './components/AutoBatchOther'
 import Transition from './components/Transition/Transition'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <AutoBatchEventHandler />
       <hr className="my-4" />
-      {/* <AutoBatchOther /> */}
+      <AutoBatchOther />
       <hr className="my-4" />
       <Transition />
     </div>
