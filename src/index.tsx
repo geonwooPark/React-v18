5cd9c44263014f8e1358b4ee21d1786e2eeefaec
박건우 <white0581@naver.com>
2023-11-23 22:20:55 +0900

    chore: React-Query 세팅 수정

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,9 @@ const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode client={queryClient}>
-    <App />
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
